e108afbea42451717498a199fc4c12f5fc5b4c05
PiotrGrzyb <piotrg6711@gmail.com>
2020-06-27 20:21:40 +0200

    Update Content.js

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -6,7 +6,6 @@ import AccountPage from './AccountPage';
 import ViewFlights from './Flights/ViewFlights';
 import OnlineBriefing from './Customer/OnlineBriefing';
 import EmployeeViewFlights from './Employee/ManageFlights/EmployeeViewFlights';
-import ManagerViewFlights from "./Manager/AdministerFights/ManagerViewFlights";
 import ViewEmployees from './Manager/AdministerEmployees/ViewEmployees';
 import ViewFiredEmployees from "./Manager/AdministerEmployees/ViewFiredEmployees";
 import CustomerReservations from './Customer/CustomerReservations';
@@ -34,8 +33,6 @@ export default function Content(props){
             return <EmployeeViewAirports accountData={props.accountData}/>;
           case "Employee View Planes":
             return <EmployeeViewPlanes accountData={props.accountData}/>;
-          case "Manager View Flights":
-            return <ManagerViewFlights accountData={props.accountData}/>;
           case "View Employees":
             return <ViewEmployees accountData={props.accountData}/>;
           case "View Fired Employees":
