cf7d699d2b72ab469bf186f7579264c091ff4cb6
arturfilipd <arturfilipd@gmail.com>
2020-07-01 17:36:32 +0200

    registration input control and style changes

--- src/Components/Authorization/Facebook.js
+++ src/Components/Authorization/Facebook.js
@@ -1,17 +1,23 @@
 import React from 'react';
 //import fbLogo from './fb-logo.png';
 import Link from '@material-ui/core/Link';
+import '../../App.css';
+import Box from "@material-ui/core/Box";
 
 export default function Facebook(){
 
     const API_BASE_URL = 'https://localhost:8443';
     const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
     const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
+    const theme = {
+        spacing: 8,
+    }
     return(
-        <div>
+
+        <Box  m={3}>
             {/*<a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
                     <img src={fbLogo} alt="Facebook" /> Log in with Facebook</a>*/}
-            <Link href={FACEBOOK_AUTH_URL}>Log in with Facebook</Link>
-        </div>
+            <Link color="whitesmoke" className={'facebookButton'} href={FACEBOOK_AUTH_URL}>Zaloguj się przez Facebook</Link>
+        </Box>
     )
 }
